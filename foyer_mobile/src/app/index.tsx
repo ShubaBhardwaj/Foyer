b668559e1bc5490da6c199db0588f204841af0a7
Shubham Bhardwaj <bhardwajshub0001@gmail.com>
2026-07-24 10:57:57 +0530

    Add mobile app

---
+++ foyer_mobile/src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-100">
+      <Text className="text-gray-900">
+        Edit src/app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
